1e9f86c49c4cda8f723fa6f5c74be9d8e5d68591
Devin Bialo <devin.bialo@gmail.com>
2024-04-06 09:41:56 -0400

    PygameTest

--- GameExecution.py
+++ GameExecution.py
@@ -4,14 +4,6 @@ from GameSim.Team import Team
 from GameSim.TeamNameGenerator import TeamNameGenerator
 
 if __name__ == '__main__':
-    #for i in range(20):
-    #    p = Player()
-    #    p.print_stats()
-
-    #for i in range(5):
-    #    g = Goalie()
-    #    g.print_stats()
-
     for i in range(5):
         t = Team()
         TeamNameGenerator.populate_team_information(t)
