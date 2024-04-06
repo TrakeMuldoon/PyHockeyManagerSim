ad0975ae77fe9423f77b4d22843cfd6ba7c4403c
Devin Bialo <devin.bialo@gmail.com>
2024-04-05 22:56:15 -0400

    Putting players on teams

--- GameSim/Team.py
+++ GameSim/Team.py
@@ -3,6 +3,14 @@ class Team:
         self.location = None
         self.team_name = None
         self.players = []
+        self.goalies = []
 
     def print_team(self):
         print(f"{self.team_name} from {self.location}")
+        print("PLAYERS")
+        for player in self.players:
+            print("\t", end='')
+            player.print_stats()
+        for goalie in self.goalies:
+            print("\t", end='')
+            goalie.print_stats()
