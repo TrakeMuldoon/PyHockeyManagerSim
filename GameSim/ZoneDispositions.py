8119bb6c8759c5039443a7fb4e7cba50d74a9d26
Devin Bialo <devin.bialo@gmail.com>
2024-04-11 14:28:11 -0400

    Added ZoneDispositions

---
+++ GameSim/ZoneDispositions.py
@@ -0,0 +1,24 @@
+class ZoneDispositions:
+    SOUTH_DEFENSIVE_ZONES = {22, 23, 24
+                            , 25, 26, 27
+                            , 29
+                            , 31, 32, 33}
+    SOUTH_NEUTRAL_ZONES = {13, 14, 15
+                           , 17
+                           , 19, 20, 21}
+    SOUTH_OFFENSIVE_ZONES = {1, 2, 3
+                             , 5
+                             , 7, 8, 9
+                             , 10, 11, 12}
+
+    NORTH_DEFENSIVE_ZONES = {1, 2, 3
+                             , 5
+                             , 7, 8, 9
+                             , 10, 11, 12}
+    NORTH_NEUTRAL_ZONES = {13, 14, 15
+                           , 17
+                           , 19, 20, 21}
+    NORTH_OFFENSIVE_ZONES = {22, 23, 24
+                            , 25, 26, 27
+                            , 29
+                            , 31, 32, 33}
